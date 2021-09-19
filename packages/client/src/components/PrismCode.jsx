0dd2068d67769e820a84440bc0a4d5c73541de2d
Dailei <szdailei@gmail.com>
2021-09-19 22:00:27 +0800

    add lineHighLight for Prism

--- packages/client/src/components/PrismCode.jsx
+++ packages/client/src/components/PrismCode.jsx
@@ -4,23 +4,37 @@ import Prism from 'prismjs';
 import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-jsx';
 import 'prismjs/components/prism-markdown';
-import 'prismjs/themes/prism-tomorrow.css';
 import 'prismjs/plugins/line-numbers/prism-line-numbers.js';
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
+import 'prismjs/plugins/line-highlight/prism-line-highlight.js';
+import './prism-tomorrow.css';
+import './prism-line-highlight.css';
 
 function PrismCode({ code, language }) {
-  let lang = language;
+  let origLang = language;
   if (!language || language === '') {
-    lang = 'jsx';
+    origLang = 'jsx';
+  }
+
+  const index = origLang.indexOf('{');
+  let lang;
+  let dataLine;
+  if (index === -1) {
+    lang = origLang.trim();
+    dataLine = null;
+  } else {
+    lang = origLang.slice(0, index).trim();
+    dataLine = origLang.slice(index + 1, origLang.length - 1);
   }
 
   useEffect(() => {
     Prism.highlightAll();
   }, []);
+
   return (
     <div>
-      <pre className={`language-${lang} line-numbers`}>
-        <code>{code}</code>
+      <pre className="line-numbers" data-line={dataLine}>
+        <code className={`language-${lang}`}>{code}</code>
       </pre>
     </div>
   );
