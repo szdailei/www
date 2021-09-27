d9e68d235b1e097aebac8c72e6ca6ceee035240c
Dailei <szdailei@gmail.com>
2021-09-27 16:56:16 +0800

    update prepare-web

--- build/prepare-web.js
+++ build/prepare-web.js
@@ -8,14 +8,16 @@ import shell from 'shelljs';
 
   const distOfWeb = path.join(dist, 'web');
   const origWeb = path.join(root, 'packages/client/dist/web');
-  const origLocalFile = path.join(root, 'packages/client/dist/local/local-presentation.html');
 
   const distOfCourses = path.join(dist, 'courses');
+  const origLocalFile = path.join(root, 'packages/client/dist/local/local-presentation.html');
   const origCourses = path.join(root, '../www-data/storage/courses');
 
   shell.mkdir(distOfWeb, distOfCourses);
   
   shell.cp('-R', origWeb, dist);
+
   shell.cp('-R', origCourses, dist);
+  // ${origLocalFile} must be copied into parent folder of ${distOfCourses}
   shell.cp(origLocalFile, dist);
 })();
